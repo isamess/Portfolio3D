3b17774aad6d2e3ead1360304d42d389b485633e
Isa Mesina <isabelmesina@gmail.com>
2023-05-22 20:54:10 -0300

    About responsive modifications

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -48,8 +48,7 @@ const Experience= ()=>{
   return (
     <>
       <motion.div variants={textVariant()}>
-      <p className={styles.sectionSubText} >What I have done so far
-      </p>
+      <p className={styles.sectionSubText} >What I have done so far</p>
       <h2 className={styles.sectionHeadText}>My work experience
       </h2>
       </motion.div>
