768f892fd0e6f10afa0e7567a7df1014eeca7b00
Isa Mesina <isabelmesina@gmail.com>
2023-06-01 13:37:17 -0300

    dist

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   base:"/portfolio3d/",
   plugins: [react()],
+  build:{
+    chunkSizeWarningLimit: 1600,
+  },
 })
